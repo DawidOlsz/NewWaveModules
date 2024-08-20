e0e54fe2ca0cc743a7a73e821b50187eec8d026c
Dawid Olszewski <dawid.olszewsk@nespresso.com>
2024-08-20 10:07:32 +0200

    plp-banner fix

--- NW.js
+++ NW.js
@@ -52,7 +52,7 @@ function PLPBanner(
                         </div>`;
 
         //$(bannerBody).insertAfter('.plp-categoryWrap:nth-child(' + positionNo + ')');
-        $(bannerBody).insertAfter('.plp-category:nth-child(' + positionNo + ')');
+        $(bannerBody).insertAfter('.plp-category').eq(positionNo - 1 );
 
 
     function copyFunction(promoCodeType) {
