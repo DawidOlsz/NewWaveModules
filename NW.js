f2976768d1f70db165236bd7404f0257e4dd653f
Dawid Olszewski <dawid.olszewsk@nespresso.com>
2024-09-02 13:34:35 +0200

    add coparation funcion

--- NW.js
+++ NW.js
@@ -88,3 +88,103 @@ function copyFunction(promoCodeType) {
         local_event_label: promoCodeType //free to fill field, please use lower case
     });
 }
+
+//Machines comparator module
+function machinesCompartor (compSku){
+    //add comprae button if it not existing
+    //comparator Exclude skus
+    var compExclude = [
+        '3694-EU-WH', 
+        '3694-EU-BK',
+        '3694-EU-RE',
+        '4194-EU2-SI-NE',
+        '4292-EU'
+    ];
+    if ( $('.compare').length <= 0 ){
+        $('.capsBox').each(function(){
+            if ( compExclude.indexOf($(this).attr('data-product-sku')) < 0 ){
+                $('<div class="compare" onclick="machinesCompartor(\'' + $(this).attr('data-product-sku') + '\')">Porównaj</div>').appendTo($(this));
+            }
+        })
+    }
+//add list to comprare 
+    var currentItem = compSku;
+
+    if ( localStorage.machCompareOne.length <= 0 ){
+        localStorage.setItem('machCompareOne', currentItem);
+    } else if ( localStorage.machCompareTwo.length <= 0 ){
+        localStorage.setItem('machCompareTwo', currentItem);
+    } else if ( localStorage.machCompareThree.length <= 0 ){
+        localStorage.setItem('machCompareThree', currentItem);
+    } else {
+        localStorage.setItem('machCompareOne', localStorage.machCompareTwo);
+        localStorage.setItem('machCompareTwo', localStorage.machCompareThree);
+        localStorage.setItem('machCompareThree', currentItem);
+    }
+
+//display comparator
+    if ( localStorage.machCompareOne.length >= 0 && $('.compWrap').length <= 0 ){
+        var comparatorBody = `
+        <div class="compWrap">
+        <div class="compInnerHead">Porównaj Ekspresy nespresso:</div>
+        <div class="compInner">
+        <div class="compItem">
+        <div class="compList compMachBody">
+        <div class="compMachBodyImg"></div>
+        <div class="ListName" AttrName="name">Model</div>
+        <div class="ListName" AttrName="sku">SKU</div>
+        </div>
+        </div>
+        </div>
+        `;
+        $(comparatorBody).appendTo('.productsListPLP');
+    }
+//render machines       
+    var toCompare = [
+        localStorage.machCompareOne,
+        localStorage.machCompareTwo,
+        localStorage.machCompareThree
+    ]
+    $('.compMachBody').remove();
+    $(toCompare).each(function(i){
+            var actualMach = toCompare[i].toString();
+            console.log('actualMach[' + ( i + 1 ) + ']: ' + actualMach);
+            $(window.CapsPLPresult).each(function(j){
+                if ( window.CapsPLPresult[j].sku === actualMach && actualMach != undefined ){
+                    var compareMachBody = `
+                    <div class="compMachBody">
+                    <div class="compMachBodyImg"><img src="${CapsPLPresult[j].image}"/></div>
+                    <div class="compMachBodyName">${CapsPLPresult[j].name}</div>
+                    <div class="compMachBodySku">${CapsPLPresult[j].sku}</div>
+                    <div class="compMachBodyFeaturesList" id="${CapsPLPresult[j].sku}"></div>
+                    <div class="compMachBodyPrice">${CapsPLPresult[j].price}</div>
+                    <div class="compMachBodyAddToBag"></div>
+                    </div>
+                    `;
+                    $(compareMachBody).appendTo('.compItem');
+                    //features list
+                    $(CapsPLPresult[j].features).each(function(g){
+                        $('<div class="ListName" AttrName="' + CapsPLPresult[j].features[g].name + '">' + CapsPLPresult[j].features[g].name + '</div>').appendTo('.compList');
+                        $('.ListName[AttrName="' + CapsPLPresult[j].features[g].name + '"]:not(:first)').remove();
+                        var compareMAchBodyFeaturesList = '';
+                        compareMAchBodyFeaturesList += '<div class="ListRow" AttrName="' + CapsPLPresult[j].features[g].name + '">';
+                        if ( CapsPLPresult[j].features[g].description === 'undefined ' ){
+                            compareMAchBodyFeaturesList += 'X';
+                        } else {
+                            compareMAchBodyFeaturesList += '' + CapsPLPresult[j].features[g].description + '';
+                        }
+                        if ( CapsPLPresult[j].features[g].unitOfMeasure === 'undefined ' ){
+                            compareMAchBodyFeaturesList += '';
+                        } else {
+                            compareMAchBodyFeaturesList += ' ' + CapsPLPresult[j].features[g].unitOfMeasure + '';
+                        }
+
+                        compareMAchBodyFeaturesList += '</div>';
+                        $(compareMAchBodyFeaturesList).appendTo('.compMachBodyFeaturesList[id="' + CapsPLPresult[j].sku + '"]')
+                    })
+                }
+            })
+    })
+}
+
+machinesCompartor ();
